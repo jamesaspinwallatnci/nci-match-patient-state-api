f9197e58b1b721b8793f654d5df6f420e1d9f56a
James Aspinwall <james.aspinwall@calamp.com>
2016-05-04 15:54:56 -0400

    removed api key from aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,6 +2,6 @@ require 'aws-sdk'
 Aws.config.update(
     {
         region: "us-east-1",
-        credentials: Aws::Credentials.new('AKIAI6MNVPDMEWKISAAQ', 'arPQU1cxo9jXwvycvHzhXTJqaa3b4Kjkh4mLCY3I'),
+        credentials: '', # it's a secret ;-)
         endpoint: "http://localhost:8000" #'us-east-1.amazonaws.com'
     });
