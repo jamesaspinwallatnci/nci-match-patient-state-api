a9d237c06caf6d44ee4bd3aae1e05fd479f3c1d0
James Aspinwall <james.aspinwall@calamp.com>
2016-05-04 16:24:12 -0400

    added patient state controller with rest route

---
+++ app/controllers/patient_state_controller.rb
@@ -0,0 +1,30 @@
+class PatientStatesController < ApplicationController
+
+    def index
+        render json: PatientState.all
+    end
+
+    def show
+        render json: PatientState.find(params['id'])
+    end
+
+    def create
+
+        id,state,processor,data = MsgParser.state_process(params)
+
+        PNet.default.load(id)
+        P[state].set(processor,data)
+
+        respond_to do |format|
+            format.json {
+                render json: P['output'].to_h
+            }
+        end
+    end
+
+    def destroy
+        PatientState.all.map &:destroy
+        Node.delete_all
+        render json: {result: 'success'}
+    end
+end
