57a8744a7406cd35d7b11c7d7db0823c27929924
James Aspinwall <james.aspinwall@calamp.com>
2016-05-04 16:20:29 -0400

    Dynamoid Patient State model

---
+++ app/models/PatientState.rb
@@ -0,0 +1,34 @@
+class PatientState
+    #include Mongoid::Document
+    #include Mongoid::Attributes::Dynamic
+
+    include Dynamoid::Document
+
+    field :registered, :serialized
+    field :msg_patient_registration, :serialized
+    field :patient_registration, :serialized
+    field :wait_for_specimen, :serialized
+    field :msg_specimen_received, :serialized
+    field :wait_for_nucleic_acid, :serialized
+    field :msg_nucleic_acid_sendout, :serialized
+    field :wait_for_variant_report, :serialized
+    field :output, :serialized
+
+    class << self
+        def destroy_all
+            self.all.each &:detroy
+        end
+
+        def delete_all
+            self.all.each &:delete
+        end
+
+    end
+
+    def unset(name)
+        self[name] = nil
+        self.save
+    end
+
+
+end
